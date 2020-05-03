2961582ed1e40d8f555f847d8d046b6097e2d7ea
Dennis Hedlund <dennishedlund@gmail.com>
2020-05-03 16:34:02 +0200

    Added some more logging for abilities, this will be moved to individual modules later

--- containers/characters.py
+++ containers/characters.py
@@ -19,7 +19,48 @@ class Character:
         logging.info(f"Character Owner: {self.attributes.owner}")
         logging.info(f"Character Name: {self.attributes.name}")
         logging.info(f"Character Money: {self.money.cp} CP; {self.money.sp} SP; {self.money.gp} GP")
-        # TODO: Add inventory printing
+        logging.info(f"""Charisma Scores: \t
+            Bonus: {self.abilities.charisma.bonus} \
+            Save: {self.abilities.charisma.save} \
+            Savemod: {self.abilities.charisma.savemod} \
+            Saveprof: {self.abilities.charisma.saveprof} \
+            Score: {self.abilities.charisma.score}""")
+
+        logging.info(f"""Constitution Scores: \t
+            Bonus: {self.abilities.constitution.bonus} \
+            Save: {self.abilities.constitution.save} \
+            Savemod: {self.abilities.constitution.savemod} \
+            Saveprof: {self.abilities.constitution.saveprof} \
+            Score: {self.abilities.constitution.score}""")
+
+        logging.info(f"""Dexterity Scores: \t
+            Bonus: {self.abilities.dexterity.bonus} \
+            Save: {self.abilities.dexterity.save} \
+            Savemod: {self.abilities.dexterity.savemod} \
+            Saveprof: {self.abilities.dexterity.saveprof} \
+            Score: {self.abilities.dexterity.score}""")
+
+        logging.info(f"""Intelligence Scores: \t
+            Bonus: {self.abilities.intelligence.bonus} \
+            Save: {self.abilities.intelligence.save} \
+            Savemod: {self.abilities.intelligence.savemod} \
+            Saveprof: {self.abilities.intelligence.saveprof} \
+            Score: {self.abilities.intelligence.score}""")
+
+        logging.info(f"""Strength Scores: \t
+            Bonus: {self.abilities.strength.bonus} \
+            Save: {self.abilities.strength.save} \
+            Savemod: {self.abilities.strength.savemod} \
+            Saveprof: {self.abilities.strength.saveprof} \
+            Score: {self.abilities.strength.score}""")
+
+        logging.info(f"""Wisdom Scores: \t
+            Bonus: {self.abilities.wisdom.bonus} \
+            Save: {self.abilities.wisdom.save} \
+            Savemod: {self.abilities.wisdom.savemod} \
+            Saveprof: {self.abilities.wisdom.saveprof} \
+            Score: {self.abilities.wisdom.score}""")
+        # TODO: Move logging to each class instead of owner class character
 
     def update(self, xmltree:ET.Element) -> None:
         logging.debug(f"Update Character: {self.attributes.name} -> Start")
